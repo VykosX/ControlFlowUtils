1e6ae4915420387a94d9229b74315ba5f5b5527b
VykosX <vykosx@gmail.com>
2024-09-17 10:41:46 -0300

    Initial support for in-place looping

--- Types.py
+++ Types.py
@@ -1,18 +1,3 @@
 class AnyType(str):
 	def __ne__(self, __value: object) -> bool:
 		return False
-
-"""
-class TautologyStr(str):
-	def __ne__(self, other):
-		return False
-
-class ByPassTypeTuple(tuple):
-	def __getitem__(self, index):
-		if index>0:
-			index=0
-		item = super().__getitem__(index)
-		if isinstance(item, str):
-			return TautologyStr(item)
-		return item
-"""
